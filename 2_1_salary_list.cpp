e419dccd994568723c9c48612c9685f34f1e7e18
Alexander Markov <markov_av_77@mail.ru>
2024-12-07 19:27:53 +0400

    2_1 Final

--- 2_1_salary_list.cpp
+++ 2_1_salary_list.cpp
@@ -11,8 +11,10 @@ int main()
     string surname;
     string name;
     string date;
+    string pay_sum;
     int sal_sum;
-    ofstream file("vedomost_1.txt");
+    ofstream file("vedomost_1.txt", ios::app);
+    file << endl;
     while (true) {
         cin >> surname;
         if (surname == "0")
@@ -22,10 +24,40 @@ int main()
         file << surname << " ";
         cin >> name;
         file << name << " ";
-        cin >> date;
+        while (true) {
+            cin >> date;
+            try {
+                int date_number = stoi(date.substr(0,2));
+                int month = stoi(date.substr(3, 2));
+                int year = stoi(date.substr(6, 4));
+                // cout << date_number << " " << month << " " << year << endl;
+                if ((date_number >= 1 && date_number <= 31) & (month >= 1 && month <= 12))
+                {
+                    break;
+                }
+                else {
+                    cout << "Incorrect date! Repeat input date of payment: "<< endl;
+                }
+            }
+            catch (const invalid_argument &e)
+            {
+                cerr << "ERROR! It's not a date! Please, repeat your input:" << endl;
+            }
+        }
         file << date << " ";
-        cin >> sal_sum;
+
+        while (true) {
+            cin >> pay_sum;
+            try {
+                sal_sum = stoi(pay_sum);
+                break;
+            }
+            catch (const invalid_argument &e) {
+            cerr << "ERROR! It's not a number! Please, repeat your input:" << endl;
+            }
+        }
         file << sal_sum << endl;
+        cout << "Next worker: " << endl;
     }
     file.close();
 }
